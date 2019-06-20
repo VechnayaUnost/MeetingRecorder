7377b909643b60c7b86d7060f4b75aa6ed778eb7
VechnayaUnost <zditovetskaya.d@yandex.ru>
2019-06-21 01:58:43 +0300

    add recyclerView, fab, layouts

--- app/src/main/java/by/darya/zdzitavetskaya/meetingrecorder/MainActivity.java
+++ app/src/main/java/by/darya/zdzitavetskaya/meetingrecorder/MainActivity.java
@@ -3,11 +3,17 @@ package by.darya.zdzitavetskaya.meetingrecorder;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import by.darya.zdzitavetskaya.meetingrecorder.presentation.list.ListFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.fl_main_container, new ListFragment()).commit();
+        }
     }
 }
---
+++ app/src/main/java/by/darya/zdzitavetskaya/meetingrecorder/presentation/list/ListFragment.java
@@ -0,0 +1,65 @@
+package by.darya.zdzitavetskaya.meetingrecorder.presentation.list;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+import by.darya.zdzitavetskaya.meetingrecorder.R;
+
+public class ListFragment extends Fragment {
+
+    Unbinder unbinder;
+
+    @BindView(R.id.rv_records)
+    RecyclerView recyclerView;
+
+    public ListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        setupRecycler();
+    }
+
+    private void setupRecycler() {
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+    }
+
+    @OnClick(R.id.floating_action_button)
+    public void fabClick() {
+        //call alertdialog
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
